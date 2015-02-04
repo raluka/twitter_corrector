025582f7b28682e091b3614748a7fdfa0e4fd78a
jun9e <mail@elenajung.de>
2015-02-04 21:29:24 +0100

    twiiter api test

--- twitter.rb
+++ twitter.rb
@@ -1,4 +1,18 @@
+require 'twitter'
+
+# See: http://www.rubydoc.info/gems/twitter
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "key1"
+  config.consumer_secret     = "key2"
+end
+
+puts client.search("railsgirls")
+
 #print question for user
+def ask_twitterhandle
+	puts "Tell me a twitterhandle!"
+	answer = gets.chomp.downcase
+end
 
 # get twitter user name
 
